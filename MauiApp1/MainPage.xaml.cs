8f044d88fa7fa0d725fa7086f71cf177fad93210
soheil.pmr <sohail.pmreza@gmail.com>
2024-12-16 12:05:56 +0330

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private async void OnUploadFileClicked(object sender, EventArgs e)
+        {
+            var result = await PickAndShowFileAsync();
+            if (!string.IsNullOrEmpty(result))
+            {
+
+            }
+        }
+
+        private async void OnStartActionClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Action", "Python Application Started", "OK");
+        }
+
+        private async Task<string> PickAndShowFileAsync()
+        {
+            try
+            {
+                var result = await FilePicker.PickAsync(new PickOptions
+                {
+                    PickerTitle = "Please Select a Title"
+                });
+                if (result is not null)
+                {
+                    var stream = await result.OpenReadAsync();
+                    using (var reader = new StreamReader(stream))
+                    {
+                        string content = await reader.ReadToEndAsync();
+                        return content;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+
+            return null;
+        }
+    }
+
+}
---
+++ MauiApp1/Services/ScreenshotService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    internal class ScreenshotService : IScreenshotService
+    {
+        public void CaptureScreen(string savePath)
+        {
+            try
+            {
+                Rectangle bounds = Screen.GetBounds(Point.Empty);
+            }
+        }
+    }
+}
